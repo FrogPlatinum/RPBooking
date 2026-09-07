be8c1ec6ca2656e0366b3bd38b72368a8f02983f
Amanda <amandajuhlsilberg@hotmail.com>
2026-09-07 11:15:31 +0200

    Made some temp styling

--- RPBookingMobile/app/index.tsx
+++ RPBookingMobile/app/index.tsx
@@ -1,16 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.element}>Edit app/index.tsx to edit this screen.</Text>
       <Text>Her starter vores SUPER seje event planner!! :D</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "purple",
+  },
+  element: {
+    color: "#a8ebeb"
+  }
+})
